c27ca121a816ab790f958bcb654174873176f417
deniseson1 <denisenson@gmail.com>
2026-02-28 02:26:24 -0700

    Create Welcome component for Google Fellowship artifact portfolio

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <div>
+            <h1>Welcome to the Google Fellowship Artifact Portfolio</h1>
+            <p>This portfolio showcases my work and projects during the Google Fellowship program.</p>
+        </div>
+    );
+};
+
+export default Welcome;
