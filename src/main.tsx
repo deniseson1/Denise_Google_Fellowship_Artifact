b6283621e868fb362ef7d99f9512839a85d87011
deniseson1 <denisenson@gmail.com>
2026-02-28 02:27:58 -0700

    Update src/main.tsx to use React 18 createRoot API

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './App.css';
 
-const App = () => <h1>Hello, React!</h1>;
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
